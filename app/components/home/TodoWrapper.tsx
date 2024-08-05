8fda71c297f09f076b049f2ed896c1938d878668
yeonna18k <yeonna18k@gmail.com>
2024-08-06 02:45:40 +0900

    fetchTodos로 데이터 받아오기, 타입 정의

--- app/components/home/TodoWrapper.tsx
+++ app/components/home/TodoWrapper.tsx
@@ -5,23 +5,41 @@ import DoneIcon from "@/public/images/done.svg";
 import React from "react";
 import { TodoRow } from "./TodoRow";
 import { DoneRow } from "./DoneRow";
+import todoApi from "@/config/axios";
+import { useQuery } from "@tanstack/react-query";
+import { ApiResponse } from "@/type";
 
 interface TodoWrapperProps {
-  isdone: boolean;
+  isDone: boolean;
 }
 
-export const TodoWrapper: React.FC<TodoWrapperProps> = ({ isdone }) => {
+const fetchTodos = async (): Promise<ApiResponse[]> => {
+  const response = await todoApi.get<ApiResponse[]>("/items");
+  return response.data;
+};
+
+export const TodoWrapper: React.FC<TodoWrapperProps> = ({ isDone: isdone }) => {
+  const todosQuery = useQuery({
+    queryKey: ["todos"],
+    queryFn: () => fetchTodos(),
+  });
+  console.log(todosQuery.data);
+
   return (
-    <div className="flex flex-col gap-y-4 mx-4 ">
+    <div className="flex flex-col gap-y-4 mx-4 mt-5 mb-10">
       {isdone ? (
         <>
           <DoneIcon />
-          <DoneRow />
+          {todosQuery?.data
+            ?.filter((item) => item.isCompleted === true)
+            .map((todo) => <DoneRow key={todo.id} name={todo.name} />)}
         </>
       ) : (
         <>
           <TodoIcon />
-          <TodoRow />
+          {todosQuery?.data
+            ?.filter((item) => item.isCompleted === false)
+            .map((todo) => <TodoRow key={todo.id} name={todo.name} />)}
         </>
       )}
     </div>
---
+++ type.ts
@@ -0,0 +1,8 @@
+export interface ApiResponse {
+  isCompleted: boolean;
+  imageUrl: string;
+  memo: string;
+  name: string;
+  tenantId: string;
+  id: number;
+}
