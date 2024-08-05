9ca4108d038ee7d3823ac76be6d96f18a57489c1
yeonna18k <yeonna18k@gmail.com>
2024-08-06 02:46:21 +0900

    분기처리 Props 수정

--- app/components/home/TodoListSection.tsx
+++ app/components/home/TodoListSection.tsx
@@ -1,25 +1,10 @@
-"use client";
-
-import todoApi from "@/config/axios";
-import { useQuery } from "@tanstack/react-query";
 import { TodoWrapper } from "./TodoWrapper";
 
-const fetchTodos = async () => {
-  const response = await todoApi.get("/items");
-  return response.data;
-};
-
 export const TodoListSection = () => {
-  const todosQuery = useQuery({
-    queryKey: ["todos"],
-    queryFn: () => fetchTodos(),
-  });
-  console.log(todosQuery.data);
-
   return (
     <>
-      <TodoWrapper isdone={false} />
-      <TodoWrapper isdone={true} />
+      <TodoWrapper isDone={false} />
+      <TodoWrapper isDone={true} />
     </>
   );
 };
