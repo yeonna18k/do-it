04493c646672c5655d8289fc9d80092487212930
yeonna18k <yeonna18k@gmail.com>
2024-08-05 23:19:38 +0900

    queryProvider 오류 수정, axios 인스턴스 이름 변경

--- config/axios.ts
+++ config/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+const todoApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, // 기본 URL 설정
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default axiosInstance;
+export default todoApi;
