e61ac6c75b8e3a4e7115f8bdf63811c2ebfe25e3
yeonna18k <yeonna18k@gmail.com>
2024-08-06 01:31:13 +0900

    AddSection 컴포넌트 css 수정

--- app/components/home/AddSection.tsx
+++ app/components/home/AddSection.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import { useState } from "react";
+import PlusIcon from "@/public/icons/plus2.svg";
 
 export const AddSection = () => {
   const [input, setInput] = useState("");
   return (
-    <div className="p-2 flex gap-2">
+    <div className="p-4 flex gap-4">
       <input
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="할 일을 입력해주세요"
-        className="w-full max-w-[1000px] h-[56px] p-5 bg-slate-100 border-2 border-black rounded-3xl shadow-xl"
+        className="w-full px-6 py-4 bg-slate-100 border-2 border-black rounded-[24px] shadow-input text-100"
       />
-      <button className="h-[56px] w-[164px] bg-slate-200 border-2 border-black rounded-3xl shadow-xl">추가하기</button>
+      <button className="p-[18px] bg-slate-200 border-2 border-black rounded-[24px]  shadow-input">
+        <PlusIcon width={16} height={16} />
+      </button>
     </div>
   );
 };
