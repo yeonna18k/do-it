ff3a4d966f6dd702ed6a4d6daf65ad68e9f51e80
yeonna18k <yeonna18k@gmail.com>
2024-08-06 02:46:42 +0900

    checkbox 호버 적용

--- app/components/home/DoneRow.tsx
+++ app/components/home/DoneRow.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import CheckedBoxIcon from "../../../public/icons/checkbox_checked.svg";
+import { ApiResponse } from "@/type";
 
-export const DoneRow = () => {
+export const DoneRow: React.FC<{ name: string }> = ({ name }) => {
   return (
     <div className="flex items-center border-2 bg-violet-100 border-black rounded-[27px] p-2 gap-4">
-      <CheckedBoxIcon />
-      <div className="text-100">은행 다녀오기</div>
+      <CheckedBoxIcon className="transition ease-linear hover:cursor-pointer hover:opacity-25 rounded-full" />
+      <div className="text-100 line-through ">{name}</div>
     </div>
   );
 };
--- app/components/home/TodoRow.tsx
+++ app/components/home/TodoRow.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import DefaultBoxIcon from "../../../public/icons/checkbox_default.svg";
 
-export const TodoRow = () => {
+export const TodoRow: React.FC<{ name: string }> = ({ name }) => {
   return (
     <div className="flex items-center border-2 border-black rounded-[27px] p-2 gap-4">
-      <DefaultBoxIcon />
-      <div className="text-100">비타민 챙겨먹기</div>
+      <DefaultBoxIcon className="transition ease-linear hover:cursor-pointer hover:opacity-25 rounded-full" />
+      <div className="text-100">{name}</div>
     </div>
   );
 };
