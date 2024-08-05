d209628895cabe2a3f29dcb77ee4be4300fd2e59
yeonna18k <yeonna18k@gmail.com>
2024-08-06 01:32:56 +0900

    TodoRow 컴포넌트 수정

--- app/components/home/TodoRow.tsx
+++ app/components/home/TodoRow.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import DefaultBoxIcon from "../../../public/icons/checkbox_default.svg";
-import CheckedBoxIcon from "../../../public/icons/checkbox_default.svg";
 
 export const TodoRow = () => {
   return (
-    <div className="flex items-center border-2 border-black rounded-3xl p-2 gap-2">
+    <div className="flex items-center border-2 border-black rounded-[27px] p-2 gap-4">
       <DefaultBoxIcon />
-
-      <div>비타민 챙겨먹기</div>
+      <div className="text-100">비타민 챙겨먹기</div>
     </div>
   );
 };
