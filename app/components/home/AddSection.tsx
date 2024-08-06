2d62b1409d22554f28375462f33851bcf151635a
yeonna18k <yeonna18k@gmail.com>
2024-08-06 18:31:43 +0900

    feat: create

--- app/components/home/AddSection.tsx
+++ app/components/home/AddSection.tsx
@@ -1,12 +1,31 @@
 "use client";
 
 import { useState } from "react";
-import { Button as Button } from "../Button";
+import { Button } from "../Button";
+import { createTodo } from "@/services/todo";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "@/config/ReactQueryClientProvider";
 
 export const AddSection = () => {
   const [input, setInput] = useState("");
+
+  const createTodoMutation = useMutation({
+    mutationFn: () => createTodo({ name: input }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+      setInput("");
+      console.log("성공");
+    },
+  });
+
   return (
-    <div className="p-4 flex gap-4 justify-center">
+    <form
+      className="p-4 flex gap-4 justify-center"
+      onSubmit={(e) => {
+        e.preventDefault();
+        createTodoMutation.mutate();
+      }}
+    >
       <input
         type="text"
         value={input}
@@ -15,7 +34,7 @@ export const AddSection = () => {
         className="w-full px-6 py-4 bg-slate-100 border-2 border-black rounded-[24px] shadow-input text-100 max-w-[1016px]"
       />
       <Button icon={"addDefault"} content={"추가하기"} bgColor={"gray"} />
-    </div>
+    </form>
   );
 };
 
