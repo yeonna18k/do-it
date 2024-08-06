7bcedd7c11de5b0b15443a10af264476c9c87b88
yeonna18k <yeonna18k@gmail.com>
2024-08-06 21:39:32 +0900

    todo 아이콘, 이미지 컴포넌트 분리

---
+++ app/components/home/EmptyTodo.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import DoneLgImg from "@/public/images/done_lg.svg";
+import TodoLgImg from "@/public/images/todo_lg.svg";
+import DoneSmImg from "@/public/images/done_sm.svg";
+import TodoSmImg from "@/public/images/todo_sm.svg";
+import TodoIcon from "@/public/images/todo.svg";
+import DoneIcon from "@/public/images/done.svg";
+import { TodoWrapperProps } from "./TodoWrapper";
+
+export const EmptyTodo = ({ isDone }: TodoWrapperProps) => {
+  return (
+    <>
+      {isDone ? <DoneIcon /> : <TodoIcon />}
+      <div className="flex flex-col justify-center items-center p-2">
+        {isDone ? (
+          <>
+            <DoneLgImg className="hidden md:block" />
+            <DoneSmImg className="md:hidden" />
+          </>
+        ) : (
+          <>
+            <TodoLgImg className="hidden md:block" />
+            <TodoSmImg className="md:hidden" />
+          </>
+        )}
+
+        <div className="flex text-100 text-center text-slate-400">
+          {isDone ? (
+            <>
+              아직 다 한 일이 없어요.
+              <br /> 해야 할 일을 체크해보세요!
+            </>
+          ) : (
+            <>
+              할 일이 없어요.
+              <br /> TODO를 새롭게 추가해주세요!
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
--- app/components/home/TodoWrapper.tsx
+++ app/components/home/TodoWrapper.tsx
@@ -2,17 +2,12 @@
 
 import React from "react";
 import { TodoRow } from "./TodoRow";
-import TodoIcon from "@/public/images/todo.svg";
-import DoneIcon from "@/public/images/done.svg";
 import { useQuery } from "@tanstack/react-query";
 import { fetchTodos } from "@/services/todo";
-import DoneLgImg from "@/public/images/done_lg.svg";
-import TodoLgImg from "@/public/images/todo_lg.svg";
-import DoneSmImg from "@/public/images/done_sm.svg";
-import TodoSmImg from "@/public/images/todo_sm.svg";
 import { ApiResponse } from "@/type";
+import { EmptyTodo } from "./EmptyTodo";
 
-interface TodoWrapperProps {
+export interface TodoWrapperProps {
   isDone: boolean;
 }
 
@@ -26,35 +21,7 @@ export const TodoWrapper = ({ isDone }: TodoWrapperProps) => {
 
   return (
     <div className="w-full lg:max-w-[588px] flex flex-col gap-y-4">
-      {isDone ? (
-        <>
-          <DoneIcon />
-          {filteredTodos?.length === 0 && (
-            <div className="flex flex-col justify-center items-center p-2">
-              <DoneLgImg className="hidden md:block" />
-              <DoneSmImg className="md:hidden" />
-              <div className="flex text-100 text-center text-slate-400">
-                아직 다 한 일이 없어요.
-                <br /> 해야 할 일을 체크해보세요!
-              </div>
-            </div>
-          )}
-        </>
-      ) : (
-        <>
-          <TodoIcon />
-          {filteredTodos?.length === 0 && (
-            <div className="flex flex-col justify-center items-center p-2">
-              <TodoLgImg className="hidden md:block" />
-              <TodoSmImg className="md:hidden" />
-              <div className="flex text-100 text-center text-slate-400">
-                할 일이 없어요.
-                <br /> TODO를 새롭게 추가해주세요!
-              </div>
-            </div>
-          )}
-        </>
-      )}
+      {filteredTodos?.length === 0 && <EmptyTodo isDone={isDone} />}
 
       {filteredTodos?.map((todo: ApiResponse) => (
         <TodoRow key={todo.id} id={todo.id} isCompleted={todo.isCompleted} name={todo.name} />
