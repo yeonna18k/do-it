d4fd6c26a4748f12eb2108b6e6692a3cf4c74f3c
yeonna18k <yeonna18k@gmail.com>
2024-08-06 20:55:16 +0900

    design: todo, done이 null 일때 기본이미지 설정

--- app/components/home/TodoWrapper.tsx
+++ app/components/home/TodoWrapper.tsx
@@ -6,6 +6,11 @@ import TodoIcon from "@/public/images/todo.svg";
 import DoneIcon from "@/public/images/done.svg";
 import { useQuery } from "@tanstack/react-query";
 import { fetchTodos } from "@/services/todo";
+import DoneLgImg from "@/public/images/done_lg.svg";
+import TodoLgImg from "@/public/images/todo_lg.svg";
+import DoneSmImg from "@/public/images/done_sm.svg";
+import TodoSmImg from "@/public/images/todo_sm.svg";
+import { ApiResponse } from "@/type";
 
 interface TodoWrapperProps {
   isDone: boolean;
@@ -17,12 +22,43 @@ export const TodoWrapper = ({ isDone }: TodoWrapperProps) => {
     queryFn: () => fetchTodos(),
   });
 
+  const filteredTodos = todosQuery?.data?.filter((item: ApiResponse) => item.isCompleted === isDone);
+
   return (
     <div className="w-full lg:max-w-[588px] flex flex-col gap-y-4">
-      {isDone ? <DoneIcon /> : <TodoIcon />}
-      {todosQuery?.data
-        ?.filter((item) => item.isCompleted === isDone)
-        .map((todo) => <TodoRow key={todo.id} id={todo.id} isCompleted={todo.isCompleted} name={todo.name} />)}
+      {isDone ? (
+        <>
+          <DoneIcon />
+          {filteredTodos?.length === 0 && (
+            <div className="flex flex-col justify-center items-center p-2">
+              <DoneLgImg className="hidden md:block" />
+              <DoneSmImg className="md:hidden" />
+              <div className="flex text-100 text-center text-slate-400">
+                아직 다 한 일이 없어요.
+                <br /> 해야 할 일을 체크해보세요!
+              </div>
+            </div>
+          )}
+        </>
+      ) : (
+        <>
+          <TodoIcon />
+          {filteredTodos?.length === 0 && (
+            <div className="flex flex-col justify-center items-center p-2">
+              <TodoLgImg className="hidden md:block" />
+              <TodoSmImg className="md:hidden" />
+              <div className="flex text-100 text-center text-slate-400">
+                할 일이 없어요.
+                <br /> TODO를 새롭게 추가해주세요!
+              </div>
+            </div>
+          )}
+        </>
+      )}
+
+      {filteredTodos?.map((todo: ApiResponse) => (
+        <TodoRow key={todo.id} id={todo.id} isCompleted={todo.isCompleted} name={todo.name} />
+      ))}
     </div>
   );
 };
