8939f4ca433f4fcd0890974c1bd608a17f20b29b
yeonna18k <yeonna18k@gmail.com>
2024-08-06 10:41:22 +0900

    refactor: DoneRow 통합 후 분기처리

--- app/components/home/TodoRow.tsx
+++ app/components/home/TodoRow.tsx
@@ -1,11 +1,32 @@
 import React from "react";
+import Link from "next/link";
 import DefaultBoxIcon from "../../../public/icons/checkbox_default.svg";
+import CheckedBoxIcon from "../../../public/icons/checkbox_checked.svg";
 
-export const TodoRow: React.FC<{ name: string }> = ({ name }) => {
+interface TodoProps {
+  id: number;
+  isCompleted: boolean;
+  name: string;
+}
+
+export const TodoRow: React.FC<TodoProps> = ({ id, isCompleted, name }) => {
+  const handleIconClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="flex items-center border-2 border-black rounded-[27px] p-2 gap-4">
-      <DefaultBoxIcon className="transition ease-linear hover:cursor-pointer hover:opacity-25 rounded-full" />
-      <div className="text-100">{name}</div>
-    </div>
+    <Link href={`/items/${id}`}>
+      <div
+        className={`flex items-center border-2 border-black rounded-[27px] p-2 gap-4 ${isCompleted ? "line-through bg-violet-100" : "bg-white"}`}
+      >
+        <div
+          className="transition ease-linear hover:cursor-pointer hover:opacity-25 rounded-full"
+          onClick={handleIconClick}
+        >
+          {isCompleted ? <CheckedBoxIcon /> : <DefaultBoxIcon />}
+        </div>
+        <div className="text-100">{name}</div>
+      </div>
+    </Link>
   );
 };
