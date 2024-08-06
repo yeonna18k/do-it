3f5f2c79171fd727cb2e2bac3ede4afec93df48a
yeonna18k <yeonna18k@gmail.com>
2024-08-06 22:39:21 +0900

    Optimistic Updates - switchTodo 적용

--- app/components/home/TodoRow.tsx
+++ app/components/home/TodoRow.tsx
@@ -5,6 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import { queryClient } from "@/config/ReactQueryClientProvider";
 import DefaultBoxIcon from "../../../public/icons/checkbox_default.svg";
 import CheckedBoxIcon from "../../../public/icons/checkbox_checked.svg";
+import { ApiResponse } from "@/type";
 
 interface TodoProps {
   id: number;
@@ -15,12 +16,19 @@ interface TodoProps {
 export const TodoRow = ({ id, isCompleted, name }: TodoProps) => {
   const handleIconClick = (e: React.MouseEvent) => {
     e.preventDefault();
-    SwitchTodoMutation.mutate(!isCompleted);
+    SwitchTodoMutation.mutate({ id, isCompleted: !isCompleted });
   };
 
   const SwitchTodoMutation = useMutation({
-    mutationFn: (isCompleted: boolean) => switchTodo(id, isCompleted),
-    onSuccess: () => {
+    mutationFn: switchTodo,
+    onMutate: async ({ id, isCompleted }: Omit<TodoProps, "name">) => {
+      await queryClient.cancelQueries({ queryKey: ["todos"] });
+      const previousTodo = queryClient.getQueryData<ApiResponse[]>(["todos"]) || [];
+      const newTodoList = previousTodo?.map((todo) => (todo.id === id ? { ...todo, isCompleted } : todo));
+      queryClient.setQueryData<ApiResponse[]>(["todos"], newTodoList);
+      return { newTodoList };
+    },
+    onSettled: () => {
       queryClient.invalidateQueries({ queryKey: ["todos"] });
     },
   });
--- services/todo.ts
+++ services/todo.ts
@@ -4,13 +4,17 @@ import { ApiResponse } from "@/type";
 interface PostTodo {
   name: string;
 }
+interface SwitchTodo {
+  id: number;
+  isCompleted: boolean;
+}
 
 export const createTodo = async (todo: PostTodo) => {
   const response = await todoApi.post("/items", todo);
   return response.data;
 };
 
-export const switchTodo = async (id: number, isCompleted: boolean) => {
+export const switchTodo = async ({ id, isCompleted }: SwitchTodo) => {
   const response = await todoApi.patch(`/items/${id}`, { isCompleted });
   return response.data;
 };
