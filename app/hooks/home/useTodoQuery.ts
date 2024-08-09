757a48d3fe9f3b401111fd4ca3b9de76c037a112
yeonna18k <yeonna18k@gmail.com>
2024-08-10 03:55:49 +0900

    useTodoQuery - isPending 추가

--- app/hooks/home/useTodoQuery.ts
+++ app/hooks/home/useTodoQuery.ts
@@ -2,16 +2,20 @@ import { fetchTodo, fetchTodos } from "@/app/services/todo";
 import { useQuery } from "@tanstack/react-query";
 
 const useTodoQuery = (id: string) => {
-  const { data: todos, error: todosError } = useQuery({
+  const { data: todos, error: isTodosError } = useQuery({
     queryKey: ["todos"],
     queryFn: () => fetchTodos(),
   });
 
-  const { data: todo, error: todoError } = useQuery({
+  const {
+    data: todo,
+    error: isTodoError,
+    isPending: isTodoPending,
+  } = useQuery({
     queryKey: ["todos", Number(id)],
     queryFn: () => fetchTodo(Number(id)),
   });
 
-  return { todos, todo };
+  return { todos, todo, isTodoPending };
 };
 export default useTodoQuery;
