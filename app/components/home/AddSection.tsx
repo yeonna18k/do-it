b9e57d1014ba11e389c6052600db747e8955c921
yeonna18k <yeonna18k@gmail.com>
2024-08-11 16:35:28 +0900

    홈 화면 버튼 분기처리

--- app/components/home/AddSection.tsx
+++ app/components/home/AddSection.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { useState } from "react";
-import useCreateTodo from "@/app/hooks/home/useCreateTodo";
 import { Button } from "../common/Button";
+import useTodoListQuery from "@/app/hooks/home/useTodoListQuery";
+import useCreateTodo from "@/app/hooks/home/useCreateTodoMutation";
 
 export const AddSection = () => {
   const [input, setInput] = useState("");
+  const { todos } = useTodoListQuery();
 
   const { createTodoMutation } = useCreateTodo(setInput);
 
   return (
     <form
-      className="p-4 flex gap-4 justify-center"
+      className="py-4 md:py-6 flex gap-2 md:gap-4 justify-center"
       onSubmit={(e) => {
         e.preventDefault();
         createTodoMutation.mutate({ name: input });
@@ -22,9 +24,14 @@ export const AddSection = () => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
         placeholder="할 일을 입력해주세요"
-        className="w-full px-6 py-4 bg-slate-100 border-2 border-black rounded-[24px] shadow-input text-100 max-w-[1016px]"
+        className="w-full px-6 py-4 bg-slate-100 border-2 border-black rounded-[24px] shadow-input text-100"
       />
-      <Button icon={"addDefault"} content={"추가하기"} bgColor={"gray"} textColor={"black"} />
+      <Button
+        icon={todos?.length === 0 ? "addPurple" : "addDefault"}
+        content={"추가하기"}
+        bgColor={todos?.length === 0 ? "purple" : "gray"}
+        textColor={todos?.length === 0 ? "white" : "black"}
+      />
     </form>
   );
 };
