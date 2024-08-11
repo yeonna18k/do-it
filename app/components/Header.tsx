f4bcfa336288740092be4e6b0d0c98b0c08700a5
yeonna18k <yeonna18k@gmail.com>
2024-08-11 16:36:03 +0900

    헤더 width, bg 변경

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <div className="border-b border-[#e9eef4] ">
-      <div className="flex py-[10px] px-4 lg:max-w-[1200px] lg:mx-auto">
+    <div className="border-b border-[#e9eef4] bg-white">
+      <div className="flex py-[10px] px-4  md:px-6 lg:mx-auto lg:px-[360px]">
         <Link href={`/`}>
           <LogoLgImg className="hidden md:block" />
           <LogoSmImg className="md:hidden" />
