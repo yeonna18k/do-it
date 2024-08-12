f951766fd9e2b3a64d8d7b1908fd50a70a55c6f7
yeonna18k <yeonna18k@gmail.com>
2024-08-12 16:47:51 +0900

    useCreateTodo - infiniteQuery 적용 후 optimistic update 적용

--- app/hooks/home/useCreateTodoMutation.ts
+++ app/hooks/home/useCreateTodoMutation.ts
@@ -3,6 +3,11 @@ import { ApiResponse } from "@/app/types/type";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React, { SetStateAction } from "react";
 
+interface TodosData {
+  pages: ApiResponse[][];
+  pageParams: any[];
+}
+
 const useCreateTodo = (setInput: React.Dispatch<SetStateAction<string>>) => {
   const queryClient = useQueryClient();
 
@@ -11,9 +16,25 @@ const useCreateTodo = (setInput: React.Dispatch<SetStateAction<string>>) => {
     onMutate: async ({ name }: { name: string }) => {
       setInput("");
       await queryClient.cancelQueries({ queryKey: ["todos"] });
-      const previousTodo = queryClient.getQueryData<ApiResponse[]>(["todos"]) || [];
-      queryClient.setQueryData(["todos"], [{ name, isCompleted: false, id: 0 }, ...previousTodo]);
-      return { previousTodo };
+      const previousPageTodo = queryClient.getQueryData<TodosData>(["todos"]);
+
+      if (previousPageTodo && Array.isArray(previousPageTodo.pages)) {
+        const previousTodo: ApiResponse[] = previousPageTodo?.pages.flat();
+        queryClient.setQueryData(["todos"], {
+          ...previousPageTodo,
+          pages: [[{ name, isCompleted: false, id: 0 }, ...previousTodo]],
+        });
+        console.log(previousTodo);
+        return { previousTodo };
+      }
+    },
+    onSuccess: (data) => {
+      console.log("mutation 성공", data);
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error) => {
+      console.log("mutation 실패", error);
+      // 여기서 에러에 따라 이전 상태로 롤백하거나 다른 처리를 할 수 있습니다.
     },
   });
   return { createTodoMutation };
