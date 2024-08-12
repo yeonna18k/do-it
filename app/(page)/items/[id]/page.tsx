8e436541ea6caa4ff417581c8921cb70fb18b4df
yeonna18k <yeonna18k@gmail.com>
2024-08-12 19:07:07 +0900

    배포 오류 수정

--- app/(page)/items/[id]/page.tsx
+++ app/(page)/items/[id]/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { DetailContainer } from "@/app/components/detail/DetailContainer";
 
-export const Detail = async ({ params }: { params: { id: string } }) => {
+export default function Detail({ params }: { params: { id: string } }) {
   return <DetailContainer id={params.id} />;
-};
-
-export default Detail;
+}
